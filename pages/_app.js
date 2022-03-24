b77ecb29088291422871bce7c186f9b22d46dd30
Michelly Hiratsuka Zorzi <43034694+HiratsukaMi@users.noreply.github.com>
2022-03-24 19:59:47 -0300

    Primeira aula de react

---
+++ pages/_app.js
@@ -0,0 +1,48 @@
+
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json';
+import Head from 'next/head';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+const theme = db.theme;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* <meta property="og:url" content= {db.url} /> */}
+        <meta property="og:title" content= {db.title} />
+        <meta property="og:image" content= {db.logo} />
+        <meta property="og:description" content= {db.description} />
+        <meta name="theme-color" content= {db.theme.colors.primary} />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
---
+++ src/components/Footer/index.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+// src/components/Footer/index.js
+const FooterWrapper = styled.footer`
+  background-color: #00000070;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  border-radius: 4px; 
+  img {
+    width: 58px;
+    margin-right: 23px;
+  }
+  a {
+    color: white;
+    text-decoration: none;
+    transition: .3s;
+    &:hover,
+    &:focus {
+      opacity: .5;
+    }
+    span {
+      text-decoration: underline;
+    }
+  }
+`;
+
+export default function Footer(props) {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <FooterWrapper {...props}>
+      <a href="https://ordemparanormal.fandom.com/wiki/Ordem_Paranormal_Wiki">
+        <img src="https://ordemparanormal.com.br/wp-content/uploads/2021/07/S3Fmbolo_Maior_ocultista-1-1-1024x1005.png" alt="Logo Ordem Paranormal" />
+      </a>
+      <p>
+        Quiz criado por: <a href="https://twitter.com/HiraMiDev">@HiraMiDev</a>
+        {' '}
+        <br></br>
+        {' '}
+        Background por: <a href="https://twitter.com/najulia_k">@najulia_k</a>
+      </p>
+    </FooterWrapper>
+  );
+}
